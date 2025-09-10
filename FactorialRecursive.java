50a36fbab79714ea61cb1e1cd55c5e48ccae773b
Gangotri527 <gangothrichandur@gmail.com>
2025-09-10 14:21:49 +0530

    Add files via upload

---
+++ FactorialRecursive.java
@@ -0,0 +1,16 @@
+public class FactorialRecursive {
+    // Recursive method to calculate factorial
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1; // Base case: factorial of 0 or 1 is 1
+        } else {
+            return n * factorial(n - 1); // Recursive call
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5; // Change this value to test other numbers
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
